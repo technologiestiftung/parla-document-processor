1dd346d2549bc056a3f75c3b8d23bee210fb3779
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-02-13 15:49:40 +0100

    feat: upper limit for importing documents set via env

--- src/importers/RedNumbers/RedNumberImporter.ts
+++ src/importers/RedNumbers/RedNumberImporter.ts
@@ -75,6 +75,16 @@ export class RedNumberImporter implements DocumentImporter {
 			}
 		}
 
+		// Limit the number of documents to import. This is done for testing and staging
+		// environment where we do not want the imported documents to grow.
+		// -1 means do not set a limit.
+		if (this.settings.maxDocumentsToImportPerDocumentType > -1) {
+			localDocuments = localDocuments.slice(
+				0,
+				this.settings.maxDocumentsToImportPerDocumentType,
+			);
+		}
+
 		const documentsInDatabase = await this
 			.sql`SELECT * FROM registered_documents where source_type = ${this.documentType}`;
 
