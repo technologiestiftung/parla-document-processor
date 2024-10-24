d540ff86445b65f870aa6a160fb2517d6e704ebe
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-10-24 14:03:48 +0200

    chore: cleanup:

--- src/run_import.ts
+++ src/run_import.ts
@@ -3,19 +3,16 @@ import { Importer } from "./importers/Importer.js";
 import { RedNumberImporter } from "./importers/RedNumbers/RedNumberImporter.js";
 import { SchrAnfrImporter } from "./importers/SchrAnfr/SchrAnfrImporter.js";
 import { WebResourcesImporter } from "./importers/WebResources/WebResourcesImporter.js";
-import { TestResourcesImporter } from "./importers/TestResources/TestResourcesImporter.js";
 
 console.log(settings);
 
 const schrAnfrImporter = new SchrAnfrImporter(settings);
-// const redNumbersImporter = new RedNumberImporter(settings);
-// const webResourcesImporter = new WebResourcesImporter(settings);
-// const testResourcesImporter = new TestResourcesImporter(settings);
+const redNumbersImporter = new RedNumberImporter(settings);
+const webResourcesImporter = new WebResourcesImporter(settings);
 const importer = new Importer([
 	schrAnfrImporter,
-	// redNumbersImporter,
-	// webResourcesImporter,
-	// testResourcesImporter,
+	redNumbersImporter,
+	webResourcesImporter,
 ]);
 
 await importer.import();
